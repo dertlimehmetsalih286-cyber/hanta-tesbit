be859ed337669139fbaa5383507037617b7e4cda
mehmet salih dertli <dertlimehmetsalih286@gmail.com>
2026-05-16 17:22:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May 16 14:29:35 2026
-
-@author: Dell
-"""
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -13,4 +7,3 @@ export default defineConfig({
     port: 5173,
   },
 });
-
