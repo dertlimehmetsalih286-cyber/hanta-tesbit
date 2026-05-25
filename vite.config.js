a7d15e641c5249c9bbb125ee962bac0818ec66de
mehmet salih dertli <dertlimehmetsalih286@gmail.com>
2026-05-25 14:11:18 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: "0.0.0.0",
-    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
-    allowedHosts: true
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
-});
+})
